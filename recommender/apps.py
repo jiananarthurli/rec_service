bf0442f63d95723e15c45c26a2f508f8a7290df9
jiananarthurli <reudnetwork@gmail.com>
2018-12-08 04:54:34 -0500

    Directly read CDF from file.

--- recommender/apps.py
+++ recommender/apps.py
@@ -9,17 +9,17 @@ class RecommenderConfig(AppConfig):
 
     def ready(self):
 
-        beta = 100
+        # beta = 100
         nearest_k = 50
 
         def load_movie_sim(filename):
             return pd.read_csv(filename, header=0, index_col=0)
 
-        def load_movie_common(filename):
-            return pd.read_csv(filename, header=0, index_col=0)
-
-        def to_CDF(i):
-            return (np.searchsorted(movie_sim_np_row, i, side='right') / movie_sim_np_row.size - 0.5) * 2
+        # def load_movie_common(filename):
+        #     return pd.read_csv(filename, header=0, index_col=0)
+        #
+        # def to_CDF(i):
+        #     return (np.searchsorted(movie_sim_np_row, i, side='right') / movie_sim_np_row.size - 0.5) * 2
 
         global movie_sim_beta
         global movie_norm
@@ -27,18 +27,20 @@ class RecommenderConfig(AppConfig):
 
         path = 'data/'
 
-        movie_sim = load_movie_sim(path + 'UR_weighted_normal_sim.csv')
-
-        movie_sim_np = movie_sim.values
-        movie_sim_np_row = movie_sim_np.reshape(1, movie_sim.shape[0] ** 2)
-        movie_sim_np_row.sort()
-        movie_sim_np_row = movie_sim_np_row[~np.isnan(movie_sim_np_row)]
+        # movie_sim = load_movie_sim(path + 'UR_weighted_normal_sim.csv')
+        #
+        # movie_sim_np = movie_sim.values
+        # movie_sim_np_row = movie_sim_np.reshape(1, movie_sim.shape[0] ** 2)
+        # movie_sim_np_row.sort()
+        # movie_sim_np_row = movie_sim_np_row[~np.isnan(movie_sim_np_row)]
 
         # this makes the similarity to be CDF of similarity
-        movie_sim = movie_sim.apply(to_CDF)
+        # movie_sim = movie_sim.apply(to_CDF)
 
-        movie_common = load_movie_common(path + 'UR_common.csv')
-        movie_sim_beta = movie_sim * (movie_common / (movie_common + beta))
+        # movie_common = load_movie_common(path + 'UR_common.csv')
+        # movie_sim_beta = movie_sim * (movie_common / (movie_common + beta))
+
+        movie_sim_beta = load_movie_sim(path + 'UR_weighted_normal_sim_CDF_beta.csv')
         print("movie_sim_beta loaded")
         movie_norm = movie_sim_beta.apply(abs).sum(axis=1)
         print("movie_norm loaded")
